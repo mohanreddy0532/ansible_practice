d00ec3d89585a3154dbc1e8c573fb66e138c7df9
mohan <mohanreddy0532@gmail.com>
2021-12-25 08:07:25 +0530

    ec2 launch with tag

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,4 +3,8 @@
 TEMP_ID="lt-0371737b9b36fe546"
 TEMP_VER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} | jq
+#First instance
+#aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} | jq
+
+#instance with Tag Name
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]"| jq
