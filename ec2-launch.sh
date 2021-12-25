916c2b49434f026f999e75862de000b0e750d1a4
mohan <mohanreddy0532@gmail.com>
2021-12-25 11:35:51 +0530

    ec2 instance update

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,8 +3,14 @@
 TEMP_ID="lt-0371737b9b36fe546"
 TEMP_VER=1
 
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Instance Already there,please check"
+  exit
+fi
+
 #First instance
 #aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} | jq
-
 #instance with Tag Name
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]"| jq
