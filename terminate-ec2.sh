169f8ece5fa52aa3c1c494e803c68c6a1ef8e337
mohan <mohanreddy0532@gmail.com>
2021-12-26 13:57:56 +0530

    terminate ec2 instances

--- terminate-ec2.sh
+++ terminate-ec2.sh
@@ -6,11 +6,11 @@ ZONE_ID=Z079880618UTU9V8KYVX0
 #aws ec2 terminate-instances --instance-ids i-5203422c
 
 
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
+#IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
 
   # Update the DNS record
-  sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record_delete.json >/tmp/record_delete.json
-  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record_delete.json | jq
+ # sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record_delete.json >/tmp/record_delete.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch record_delete.json | jq
 
   sleep 20
   #delete ec2
